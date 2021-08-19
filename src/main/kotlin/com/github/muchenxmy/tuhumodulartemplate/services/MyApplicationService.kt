dce15c2ba1711baf535908d83a053c232993582f
GitHub Action <action@github.com>
2021-08-19 10:13:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/muchenxmy/tuhumodulartemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.muchenxmy.tuhumodulartemplate.services
+
+import com.github.muchenxmy.tuhumodulartemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/muchenxmy/tuhumodulartemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/muchenxmy/tuhumodulartemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.muchenxmy.tuhumodulartemplate.services
 
+import com.github.muchenxmy.tuhumodulartemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
